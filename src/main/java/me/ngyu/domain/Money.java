9568103563c5e7c02dfa62d12be6f66d80115115
nGyu <vpdls1511@gmail.com>
2026-06-07 16:52:05 +0900

    feat : Money 도메인 로직 추가

--- src/main/java/me/ngyu/domain/Money.java
+++ src/main/java/me/ngyu/domain/Money.java
@@ -14,25 +14,29 @@ public record Money(BigDecimal amount,
     if (currency == null || currency.isBlank()) {
       throw new MoneyValidateException("currency는 비어 있을 수 없습니다.");
     }
-
-    if (amount.compareTo(BigDecimal.ZERO) < 0) {
-      throw new MoneyValidateException("amount는 음수일 수 없습니다.");
-    }
   }
 
   public static Money of(long amount) {
     return Money.of(amount, "KRW");
   }
 
+  public static Money of(Long amount) {
+    return Money.of(amount, "KRW");
+  }
+
   public static Money of(long amount, String currency) {
-    return new Money(BigDecimal.valueOf(amount), currency);
+    return Money.of(BigDecimal.valueOf(amount), currency);
+  }
+
+  public static Money of(double amount) {
+    return Money.of(amount, "KRW");
   }
 
   public static Money of(Double amount) {
     return Money.of(amount, "KRW");
   }
 
-  public static Money of(Double amount, String currency) {
+  public static Money of(double amount, String currency) {
     return new Money(BigDecimal.valueOf(amount), currency);
   }
 
@@ -44,8 +48,38 @@ public record Money(BigDecimal amount,
     return new Money(amount, currency);
   }
 
+  public Money add(Money other) {
+    validOperation(other);
+    return Money.of(amount.add(other.amount), this.currency);
+  }
+
+  public Money minus(Money other) {
+    validOperation(other);
+    return Money.of(amount.subtract(other.amount), this.currency);
+  }
+
+  public Money multiply(Money other) {
+    validOperation(other);
+    return Money.of(amount.multiply(other.amount), this.currency);
+  }
+
+  public Money divide(Money other) {
+    validOperation(other);
+    return Money.of(amount.divide(other.amount), this.currency);
+  }
+
   public String get() {
     return amount.toString() + " " + currency;
   }
 
+
+  private void validOperation(Money other) {
+    if (other == null) {
+      throw new MoneyValidateException("연산 대상자는 null일 수 없습니다.");
+    }
+
+    if(!this.currency.equals(other.currency)) {
+      throw new MoneyValidateException("통화가 다릅니다.");
+    }
+  }
 }
