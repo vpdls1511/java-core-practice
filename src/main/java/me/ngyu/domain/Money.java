56889633fea7111d17cd89c22a6b903d9809e59c
nGyu <vpdls1511@gmail.com>
2026-06-07 16:33:19 +0900

    feat : Money 생성 규칙 생성

--- src/main/java/me/ngyu/domain/Money.java
+++ src/main/java/me/ngyu/domain/Money.java
@@ -5,8 +5,28 @@ import java.math.BigDecimal;
 public record Money(BigDecimal amount,
                     String currency) {
 
-  public static Money of(int amount, String currency) {
+  public static Money of(long amount) {
+    return Money.of(amount, "KRW");
+  }
+
+  public static Money of(long amount, String currency) {
     return new Money(BigDecimal.valueOf(amount), currency);
+  }
+
+  public static Money of(Double amount) {
+    return Money.of(amount, "KRW");
+  }
+
+  public static Money of(Double amount, String currency) {
+    return new Money(BigDecimal.valueOf(amount), currency);
+  }
+
+  public static Money of(BigDecimal amount) {
+    return Money.of(amount, "KRW");
+  }
+
+  public static Money of(BigDecimal amount, String currency) {
+    return new Money(amount, currency);
   }
 
   public String get() {
