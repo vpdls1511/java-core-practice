016bc033188634b85f4cb840b9f0fbeeb84520ff
nGyu <vpdls1511@gmail.com>
2026-06-07 15:25:00 +0900

    feat : Money 도메인 생성

--- src/main/java/me/ngyu/domain/Money.java
+++ src/main/java/me/ngyu/domain/Money.java
@@ -1,5 +1,16 @@
 package me.ngyu.domain;
 
-public class Money {
+import java.math.BigDecimal;
+
+public record Money(BigDecimal amount,
+                    String currency) {
+
+  public static Money of(int amount, String currency) {
+    return new Money(BigDecimal.valueOf(amount), currency);
+  }
+
+  public String get() {
+    return amount.toString() + " " + currency;
+  }
 
 }
