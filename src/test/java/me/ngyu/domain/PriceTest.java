80d1ed7689d6760f5ec7cbdfbb1f911aca4e77bb
nGyu <vpdls1511@gmail.com>
2026-06-09 20:44:52 +0900

    test : Price 도메인 테스트

---
+++ src/test/java/me/ngyu/domain/PriceTest.java
@@ -0,0 +1,33 @@
+package me.ngyu.domain;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.math.BigDecimal;
+import me.ngyu.exception.MoneyValidateException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class PriceTest {
+
+  @Test
+  @DisplayName("Price는 Null이어서는 안된다")
+  void priceIsNotNull() {
+    assertThrows(MoneyValidateException.class, () -> Price.of(null));
+  }
+
+  @Test
+  @DisplayName("Price는 음수여서는 안된다 - fail")
+  void priceIsNegative() {
+    assertThrows(MoneyValidateException.class, () -> Price.of(Money.of(-1000)));
+  }
+
+  @Test
+  @DisplayName("Price는 음수여서는 안된다 - success")
+  void priceIsNotNegative() {
+    Price price = Price.of(Money.of(1000));
+
+    assertTrue(price.money().amount().compareTo(BigDecimal.ZERO) > 0);
+  }
+
+}
